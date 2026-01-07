43295401e8ae5fff80fe574ad45644570d8dc271
Richard Garcia <code.richardy@gmail.com>
2026-01-07 00:00:49 -0300

    feat: introduces SidebarLabels constants for navigation in the sidebar

---
+++ Source/Constants/SidebarLabels.cs
@@ -0,0 +1,8 @@
+namespace Vinder.Comanda.Merchants.WebUI.Constants;
+
+public static class SidebarLabels
+{
+    public const string Orders = "Pedidos";
+    public const string Payments = "Pagamentos";
+    public const string Products = "Produtos";
+}
