ffd839f519c8d5d97ecb98b3b83aa95935c0c12a
Richard Garcia <code.richardy@gmail.com>
2026-01-08 22:55:05 -0300

    feat: this commit introduces authentication extension for configuring authentication services

---
+++ Source/Extensions/AuthenticationExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace Vinder.Comanda.Merchants.WebUI.Extensions;
+
+public static class AuthenticationExtension
+{
+    public static void AddAuthentication(this IServiceCollection services)
+    {
+        services.AddAuthorizationCore();
+        services.AddCascadingAuthenticationState();
+
+        services.AddScoped<ISessionManager, SessionManager>();
+        services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
+        services.AddScoped<AuthenticationInterceptor, AuthenticationInterceptor>();
+    }
+}
