9649caf908e3a171c8d062e3996250bea36548c4
Richard Garcia <code.richardy@gmail.com>
2026-01-08 22:52:15 -0300

    feat: this commit introduces ILocalStorageGateway interface for local storage operations

---
+++ Source/Contracts/ILocalStorageGateway.cs
@@ -0,0 +1,13 @@
+﻿namespace Vinder.Comanda.Merchants.WebUI.Contracts;
+
+public interface ILocalStorageGateway
+{
+    public Task<T?> GetAsync<T>(string key, CancellationToken cancellation = default);
+    public Task<string?> GetAsStringAsync(string key, CancellationToken cancellation = default);
+
+    public Task SetAsync<T>(string key, T value, CancellationToken cancellation = default);
+    public Task SetAsStringAsync(string key, string value, CancellationToken cancellation = default);
+
+    public Task RemoveAsync(string key, CancellationToken cancellation = default);
+    public Task ClearAsync(CancellationToken cancellation = default);
+}
