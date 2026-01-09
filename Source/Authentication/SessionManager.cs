687b77c0efe8b8a7ce523c5885d21211221a5e16
Richard Garcia <code.richardy@gmail.com>
2026-01-08 22:48:19 -0300

    feat: this commit introduces session manager for handling user authentication sessions

---
+++ Source/Authentication/SessionManager.cs
@@ -0,0 +1,27 @@
+﻿namespace Vinder.Comanda.Merchants.WebUI.Authentication;
+
+public sealed class SessionManager(ILocalStorageGateway localStorage, IIdentityClient identityClient, AuthenticationStateProvider provider) : ISessionManager
+{
+    public async Task SignInAsync(string token)
+    {
+        await localStorage.SetAsStringAsync(Storage.SecurityToken, token);
+
+        // inform provider of signin because blazor does not automatically detect localstorage updates
+        if (provider is JwtAuthenticationStateProvider authenticationState)
+            authenticationState.NotifyAuthenticationStateChanged();
+    }
+
+    public async Task SignOutAsync()
+    {
+        var refreshToken = await localStorage.GetAsStringAsync(Storage.RefreshToken);
+        if (string.IsNullOrWhiteSpace(refreshToken))
+            return;
+
+        await localStorage.RemoveAsync(Storage.SecurityToken);
+        await identityClient.InvalidateSessionAsync(new() { RefreshToken =  refreshToken });
+
+        // notify provider of logout because blazor does not detect localstorage changes automatically
+        if (provider is JwtAuthenticationStateProvider authenticationState)
+            authenticationState.NotifyAuthenticationStateChanged();
+    }
+}
