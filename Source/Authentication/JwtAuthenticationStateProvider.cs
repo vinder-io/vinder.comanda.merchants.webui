f326d2faf537c3e04c73c0cda9a82c498258e5a1
Richard Garcia <code.richardy@gmail.com>
2026-01-08 22:50:46 -0300

    feat: introduce jwt authentication state provider for handling JWT authentication state

---
+++ Source/Authentication/JwtAuthenticationStateProvider.cs
@@ -0,0 +1,34 @@
+﻿namespace Vinder.Comanda.Merchants.WebUI.Authentication;
+
+public sealed class JwtAuthenticationStateProvider(ILocalStorageGateway localStorage) :
+    AuthenticationStateProvider
+{
+    private readonly JwtSecurityTokenHandler tokenHandler = new();
+    private readonly ClaimsPrincipal anonymous =
+        new(new ClaimsIdentity());
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var tokenString = await localStorage.GetAsync<string>(Storage.SecurityToken);
+        if (string.IsNullOrWhiteSpace(tokenString))
+        {
+            return new AuthenticationState(anonymous);
+        }
+
+        var token = tokenHandler.ReadJwtToken(tokenString);
+        if (token.ValidTo < DateTime.UtcNow)
+        {
+            return new AuthenticationState(anonymous);
+        }
+
+        var identity = new ClaimsIdentity(token.Claims, AuthenticationDefaults.Type);
+        var principal = new ClaimsPrincipal(identity);
+
+        return new AuthenticationState(principal);
+    }
+
+    public void NotifyAuthenticationStateChanged()
+    {
+        NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+    }
+}
