f2170de01f8208164d496f00bcc43f3857a043ec
Richard Garcia <code.richardy@gmail.com>
2026-01-08 22:54:49 -0300

    feat: introduce authentication interceptor for handling authorization tokens

---
+++ Source/Http/Interceptors/AuthenticationInterceptor.cs
@@ -0,0 +1,16 @@
+﻿namespace Vinder.Comanda.Merchants.WebUI.Http.Interceptors;
+
+public sealed class AuthenticationInterceptor(ILocalStorageGateway localStorage) : DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request, CancellationToken cancellation)
+    {
+        var token = await localStorage.GetAsStringAsync(Storage.SecurityToken, cancellation);
+
+        /* https://www.rfc-editor.org/rfc/rfc6750 */
+        if (!string.IsNullOrWhiteSpace(token))
+            request.Headers.Authorization = new(AuthenticationDefaults.Scheme, token);
+
+        return await base.SendAsync(request, cancellation);
+    }
+}
