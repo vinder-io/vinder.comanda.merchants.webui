649859c7fe4f525dc860a7111ec2fccb297df44c
Richard Garcia <code.richardy@gmail.com>
2026-01-08 22:51:51 -0300

    feat: introduce storage class for defining security and refresh token constants

---
+++ Source/Constants/Storage.cs
@@ -0,0 +1,7 @@
+﻿namespace Vinder.Comanda.Merchants.WebUI.Constants;
+
+public sealed class Storage
+{
+    public const string SecurityToken = "comanda:security-token";
+    public const string RefreshToken = "comanda:refresh-token";
+}
