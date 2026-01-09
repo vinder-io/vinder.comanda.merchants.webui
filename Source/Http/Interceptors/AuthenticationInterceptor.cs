6a61f9d2d5adcf43c4493191b86a0f3df10f3641
Richard Garcia <code.richardy@gmail.com>
2026-01-08 22:59:54 -0300

    fix: correct parameter name for cancellation token in authentication interceptor

--- Source/Http/Interceptors/AuthenticationInterceptor.cs
+++ Source/Http/Interceptors/AuthenticationInterceptor.cs
@@ -3,14 +3,14 @@
 public sealed class AuthenticationInterceptor(ILocalStorageGateway localStorage) : DelegatingHandler
 {
     protected override async Task<HttpResponseMessage> SendAsync(
-        HttpRequestMessage request, CancellationToken cancellation)
+        HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        var token = await localStorage.GetAsStringAsync(Storage.SecurityToken, cancellation);
+        var token = await localStorage.GetAsStringAsync(Storage.SecurityToken, cancellationToken);
 
         /* https://www.rfc-editor.org/rfc/rfc6750 */
         if (!string.IsNullOrWhiteSpace(token))
             request.Headers.Authorization = new(AuthenticationDefaults.Scheme, token);
 
-        return await base.SendAsync(request, cancellation);
+        return await base.SendAsync(request, cancellationToken);
     }
 }
