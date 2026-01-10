87edd13d504888f72c80b8eb812c4ad6f3e86176
Richard Garcia <code.richardy@gmail.com>
2026-01-10 14:23:14 -0300

    fix: correct constant names for merchant, establishment, and user in Storage class

--- Source/Constants/Storage.cs
+++ Source/Constants/Storage.cs
@@ -4,7 +4,7 @@ public static class Storage
 {
     public const string SecurityToken = "comanda:security-token";
     public const string RefreshToken = "comanda:refresh-token";
-    public const string MerchantId = "comanda:merchant";
-    public const string EstablishmentId = "comanda:establishment";
-    public const string UserId = "comanda:user";
+    public const string Merchant = "comanda:merchant";
+    public const string Establishment = "comanda:establishment";
+    public const string User = "comanda:user";
 }
