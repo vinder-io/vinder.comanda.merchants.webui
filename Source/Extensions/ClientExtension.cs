c66521ac40ccb6e29d420b0c59f599eeae585a40
Richard Garcia <code.richardy@gmail.com>
2026-01-10 13:44:50 -0300

    refactor method to remove unused product client and streamline http client registration

--- Source/Extensions/ClientExtension.cs
+++ Source/Extensions/ClientExtension.cs
@@ -5,23 +5,22 @@ public static class ClientExtension
     public static void AddClients(this IServiceCollection services, IConfiguration configuration)
     {
         var address = configuration.GetValue<string>("Settings:Gateway")!;
-
-        services.AddHttpClient<IEstablishmentClient, EstablishmentClient>(client =>
+        var storeClient = services.AddHttpClient<IStoreClient, StoreClient>(client =>
         {
             client.BaseAddress = new Uri(address);
             client.Timeout = TimeSpan.FromMinutes(minutes: 1, seconds: 30);
         });
 
-        services.AddHttpClient<IOrderClient, OrderClient>(client =>
+        var orderClient = services.AddHttpClient<IOrderClient, OrderClient>(client =>
         {
             client.BaseAddress = new Uri(address);
             client.Timeout = TimeSpan.FromMinutes(minutes: 1, seconds: 30);
         });
 
-        services.AddHttpClient<IProductClient, ProductClient>(client =>
-        {
-            client.BaseAddress = new Uri(address);
-            client.Timeout = TimeSpan.FromMinutes(minutes: 1, seconds: 30);
-        });
+        // ensure an authentication interceptor is always registered for http clients
+        // every http client must include a message handler responsible for authentication
+
+        storeClient.AddHttpMessageHandler<AuthenticationInterceptor>();
+        orderClient.AddHttpMessageHandler<AuthenticationInterceptor>();
     }
 }
