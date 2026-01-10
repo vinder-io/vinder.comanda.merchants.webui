5f67c85984bdde8f5537a2396052e44f1420fa1a
Richard Garcia <code.richardy@gmail.com>
2026-01-10 12:47:12 -0300

    feat: this commit introduces additional constants for MerchantId, EstablishmentId, and UserId in Storage class

--- Source/Constants/Storage.cs
+++ Source/Constants/Storage.cs
@@ -4,4 +4,7 @@ public static class Storage
 {
     public const string SecurityToken = "comanda:security-token";
     public const string RefreshToken = "comanda:refresh-token";
+    public const string MerchantId = "comanda:merchant";
+    public const string EstablishmentId = "comanda:establishment";
+    public const string UserId = "comanda:user";
 }
