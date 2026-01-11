a8735b879cd64bba936dc1f66011651caa6c2ed0
Richard Garcia <code.richardy@gmail.com>
2026-01-11 15:58:49 -0300

    this commit replace 'user' constant with 'principal' in storage class

--- Source/Constants/Storage.cs
+++ Source/Constants/Storage.cs
@@ -6,5 +6,5 @@ public static class Storage
     public const string RefreshToken = "comanda:refresh-token";
     public const string Merchant = "comanda:merchant";
     public const string Establishment = "comanda:establishment";
-    public const string User = "comanda:user";
+    public const string Principal = "comanda:principal";
 }
